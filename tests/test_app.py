2d2c93ce4f5f16aad4437d49770e7a37049bf2a5
abraham-siyanbola <abrahamsiyanbola09@gmail.com>
2026-01-30 01:06:39 +0100

    All the manifest are  provided in the jenkinsfile

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,4 +2,4 @@ from app import app  # Assuming you update app.py to importable
 def test_hello():
     with app.test_client() as client:
         response = client.get('/')
-        assert response.data.decode('utf-8') == b"Hello World from Siyanbola Abraham! 🚀"
+        assert response.data.decode('utf-8') == "Hello World from Siyanbola Abraham! 🚀"
