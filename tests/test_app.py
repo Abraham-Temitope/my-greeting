feef81359df90fea46268b6e1eca5011d6cd86e4
abraham-siyanbola <abrahamsiyanbola09@gmail.com>
2026-01-30 02:05:59 +0100

    commit the test app

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
-from app import app  # Assuming you update app.py to importable
+from app import app
+
 def test_hello():
     with app.test_client() as client:
         response = client.get('/')
+        # Fix: compare strings after decoding
         assert response.data.decode('utf-8') == "Hello World from Siyanbola Abraham! 🚀"
