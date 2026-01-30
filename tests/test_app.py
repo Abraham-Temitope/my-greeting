a6e42c88fe0d98e4e987c76fdd1797320695f8e1
abraham-siyanbola <abrahamsiyanbola09@gmail.com>
2026-01-30 02:47:32 +0100

    the new fix, jenkins

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,6 @@
-from app import app
+from ..app import app   # relative import from tests/ to root
 
 def test_hello():
     with app.test_client() as client:
         response = client.get('/')
-        # Fix: compare strings after decoding
         assert response.data.decode('utf-8') == "Hello World from Siyanbola Abraham! 🚀"
