e152ab5b159a3df873f35c88fb5ebd701e240c81
abraham-siyanbola <abrahamsiyanbola09@gmail.com>
2026-01-30 11:50:13 +0100

    my jenkinsfile for CI/CD

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,13 @@
-from ..app import app   # relative import from tests/ to root
+import sys
+import os
+
+# Add project root to Python path so 'app' is importable
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import app
 
 def test_hello():
     with app.test_client() as client:
         response = client.get('/')
         assert response.data.decode('utf-8') == "Hello World from Siyanbola Abraham! 🚀"
+
